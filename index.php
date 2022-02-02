b96c2823c350163c3313a9cd44bbdb0dbaf77faf
dgtyPedro <pedrogcs0104@gmail.com>
2022-02-02 10:16:43 -0300

    feat: Front-End finalizado

--- index.php
+++ index.php
@@ -52,27 +52,107 @@ if ($_POST && $_POST['password'] == $senha){
     $action = $conn2->query($sql2);  
     
     $books = [
-        ['Número de Candidatos', 'Número de Empregadores', 'Novos Candidatos', 'Novos Empregadores'],
-        [$candidatos, $empregadores,  $novoc, $novoe]
+        ['N煤mero de Candidatos', 'N煤mero de Empregadores', 'Novos Candidatos', 'Novos Empregadores'],
+        [$candidatos, $empregadores, $novoc, $novoe]
     ];
     $xlsx = SimpleXLSXGen::fromArray( $books );
-    $xlsx->downloadAs('Relatório.xlsx'); 
+    $xlsx->downloadAs('Report.xlsx'); 
     
 }
 ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
+<style>
+html {
+    background: #083c64;
+    color:white;
+}
+
+input[type="password"] {
+    font-size:30px;
+}
+
+body {
+  width: 500px;
+  margin: 0 auto;
+  padding-top: 30vh;
+  font-family: Tahoma, sans-serif;
+  
+}
+
+fieldset {
+  position: relative;
+  display: inline-block;
+  padding: 0 0 0 40px;
+  background: #fff;
+  border: none;
+  border-radius: 5px;
+  
+}
+
+input,
+button {
+  position: relative;
+  
+  height: 50px;
+  padding: 0;
+  display: inline-block;
+  float: left;
+  
+}
+
+input {
+    width: 300px;
+  color: #666;
+  z-index: 2;
+  border: 0 none;
+}
+input:focus {
+  outline: 0 none;
+}
+
+
+button {
+  z-index: 1;
+  width: 200px;
+  border: 0 none;
+  background: #fcb434;
+  cursor: pointer;
+  border-radius: 0 5px 5px 0;  
+  color: #083c64;
+  font-size:20px;
+  font-weight: bolder;
+  /* -webkit-transform: translate(-50px, 0);
+      -ms-transform: translate(-50px, 0);
+          transform: translate(-50px, 0);
+  -webkit-transition-duration: 0.3s;
+          transition-duration: 0.3s; */
+}
+a{
+    text-decoration: none;
+    color: white;
+}
+
+a:visited{
+    text-decoration: none;
+    color: white;
+}
+
+</style>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Relatório Varejo Contrata</title>
+    <title>Relat贸rio Varejo Contrata</title>
 </head>
 <body>
+<h1>Relat贸rios Varejo Contrata</h1>
     <form method="post">
-        <p>Insert password</p>
-        <input type="text" name="password"/>
-        <input type="submit"/>
+        
+        <p>Insira a senha / <a href="https://varejocontrata.com.br/wp-admin/">Voltar</a></p>
+        
+        <input type="password" name="password"/>
+        <button type="submit">Gerar</button>
     </form>
 </body>
 </html>
